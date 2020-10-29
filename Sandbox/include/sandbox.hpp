ad2c93cd224a95a1dbdf19127b583bb0bd479051
Adrian Licnerski <licnerskiadrian@gmail.com>
2020-10-29 20:27:23 +0100

    Sandbox stuff, rebase to test.
    - Fixes for new Event System
    - Added mouse key callback to test mouse events
    - Fixes for new params
    - Started moving things back to renderer
    - Colour/Texture block
    - Added some collision detection
    - Added handle to Events, so it's not screaming warnings
    - Split the sandbox into cpp/hpp
    - Made some collision test stuff, which will be used later for the game
    - Allowed multiple "balls" to be added, with some destruction properties
    - Integrated some api simplifications
    - Finished up the game for sandbox
    - renderer uses both texture and flat colour
    - new rendering by pushing drawableAdd events
    - fixed typos
    - added text display with scores
    - added reset button
    - moved from premake to cmake

---
+++ Sandbox/include/sandbox.hpp
@@ -0,0 +1,59 @@
+#include "Application/MyEngine.hpp"
+#include "Physics/body2D.hpp"
+#include "Physics/Events/collisionEvent.hpp"
+#include "Renderer/text.hpp"
+
+#include "platform.hpp"
+#include "ball.hpp"
+#include "breakableBlock.hpp"
+
+#include <glm/glm.hpp>
+#include <memory>
+#include <iostream>
+
+class Sandbox : public MyEngine::Application
+{
+public:
+    Sandbox(std::string title, int w, int h) : 
+        MyEngine::Application(title, w, h, false, false, 1 / 120.0f) { }
+
+    bool onWindowCreate() override;
+    bool onRender() override;
+    bool onUpdate(float t_deltaTime) override;
+
+private: 
+    void setupPlatform();
+    void setupBlocks();
+    void addBall(unsigned int t_x, unsigned int t_y);
+    void setupWindowCollision();
+    void setupText();
+
+    unsigned int m_maxScore = 1;
+    unsigned int m_blockSize;
+    unsigned int m_lastId = 0;
+
+    std::shared_ptr<MyEngine::Renderer::Text> m_maxBallCount;
+    std::shared_ptr<MyEngine::Renderer::Text> m_ballCount;
+    std::shared_ptr<MyEngine::Renderer::Text> m_currentScoreText;
+    std::shared_ptr<MyEngine::Renderer::Text> m_maxScoreText;
+
+
+    std::shared_ptr<Platform> m_platform;
+    std::vector<std::shared_ptr<Ball>> m_balls;
+    std::vector<std::string> m_ballGroups;
+    std::vector<std::shared_ptr<BreakableBlock>> m_breakableBlocks;
+
+    std::shared_ptr<MyEngine::Renderer::Material> m_material;
+    std::shared_ptr<MyEngine::Renderer::Material> m_ballMaterial;
+    std::shared_ptr<MyEngine::Renderer::Mesh2D> m_ballMesh;
+
+    std::string m_windowGroup = "window";
+    std::string m_breakableBlockGroup = "Block-Ball";
+    std::shared_ptr<MyEngine::Physics::Body2D> m_windowHitbox;
+};
+
+
+std::unique_ptr<MyEngine::Application> MyEngine::CreateApplication()
+{
+    return std::make_unique<Sandbox>("Very creative title!", 1080, 720);
+}
