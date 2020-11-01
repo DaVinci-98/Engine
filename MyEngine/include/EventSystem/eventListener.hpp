7f66f0a18bf73758009abf75add2211ccf589c29
Adrian Licnerski <licnerskiadrian@gmail.com>
2020-11-01 14:24:07 +0100

    Added function for removing next listeners.

--- MyEngine/include/EventSystem/eventListener.hpp
+++ MyEngine/include/EventSystem/eventListener.hpp
@@ -14,19 +14,17 @@ namespace MyEngine::EventSystem
     {
     public:
         virtual void dispatch(EventType&& t_event) = 0;
-        inline void registerNextListener(EventListener<EventType>& t_listner)
-        {
-            m_nextDispatcher = [&t_listner](EventType&& t_event) -> void { t_listner -> dispatch(std::move(t_event)); };
-        }
         inline void registerEmitter(EventEmitter<EventType>& t_emitter)
         {
             auto dispatcher = [this](EventType&& t_event) -> void { this -> dispatch(std::move(t_event)); };
             t_emitter.registerDispatcher(std::move(dispatcher));
         }
+        inline void registerNextListener(EventListener<EventType>& t_listner)
+            { m_nextDispatcher = [&t_listner](EventType&& t_event) -> void { t_listner->dispatch(std::move(t_event)); };}
         inline void deleteCallback(unsigned int t_key)
-        { 
-            m_callbackMap.erase(t_key); 
-        }
+            { m_callbackMap.erase(t_key); }
+        inline void deleteNextListener()
+            { m_nextDispatcher = nullptr; }
     protected:
         EventListener() { }
         void registerCallback(unsigned int t_key, std::function<void(EventType&)>&& t_callback)
@@ -35,13 +33,9 @@ namespace MyEngine::EventSystem
             m_callbackMap.insert(std::move(pair)); 
         }
         inline std::function<void(EventType&)>& callback(unsigned int t_key)
-        { 
-            return m_callbackMap[t_key]; 
-        }
+            { return m_callbackMap[t_key]; }
         inline std::function<void(EventType&&)>& nextDispatcher()
-        { 
-            return m_nextDispatcher;
-        }
+            { return m_nextDispatcher;}
     private:
         std::function<void(EventType&&)> m_nextDispatcher;
         std::unordered_map<unsigned int, std::function<void(EventType&)>> m_callbackMap;
